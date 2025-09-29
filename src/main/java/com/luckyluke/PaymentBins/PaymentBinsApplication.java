6a6fe885982a8b37d9f4a27ecfbe3c4acdde852a
lukeclement <masterclement@gmail.com>
2025-09-29 20:26:52 +0100

    Adding literally everything to get stuff setup

---
+++ src/main/java/com/luckyluke/PaymentBins/PaymentBinsApplication.java
@@ -0,0 +1,13 @@
+package com.luckyluke.PaymentBins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentBinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentBinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckyluke/PaymentBins/PaymentBinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckyluke.PaymentBins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentBinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
